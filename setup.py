36b889f383e0f57e07d08a1828810f1b2b856434
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2020-05-15 17:13:33 +0100

    Inc. setup.py file

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.1.1'
+DISTNAME = 'ARNA'
+DESCRIPTION = "Analysis and operational code used for the ARNA campaign"
+AUTHOR = 'Tomas Sherwen'
+AUTHOR_EMAIL = 'tomas.sherwen@york.ac.uk'
+URL = 'https://github.com/tsherwen/ARNA'
+LICENSE = 'MIT'
+PYTHON_REQUIRES = '>=3.5'
+
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    INSTALL_REQUIRES = []
+else:
+    INSTALL_REQUIRES = [
+                        'affine',
+                        'cartopy',
+                        'geopandas',
+                        'matplotlib',
+                        'netcdf4',
+                        'numpy',
+                        'pandas',
+                        'pytest',
+                        'rasterio',
+                        'scipy',
+                        'xarray'
+                       ]
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering',
+]
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name=DISTNAME,
+      version=VERSION,
+      license=LICENSE,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      classifiers=CLASSIFIERS,
+      description=DESCRIPTION,
+      long_description=readme(),
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      url=URL,
+      packages=find_packages())
