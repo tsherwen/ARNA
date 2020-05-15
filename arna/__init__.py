2d1caf24844d39dc88ee4e9fdb5e4afbc3f442b4
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2020-05-15 17:13:18 +0100

    add init.py and rename module

--- arna/__init__.py
+++ arna/__init__.py
@@ -1,1 +1,9 @@
-from . import core
+from core import *
+from campaign_operational_driver import *
+from GEOS import * 
+from campaign_misc import *
+from plotting_fcast import *
+from observations import * .py           
+from plotting import * .py        
+from utils import *
+
