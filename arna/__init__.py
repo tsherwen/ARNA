9d88f1874cc48e9420ab27963d37524eac64320c
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2020-05-15 17:24:56 +0100

    Fix typos in init.py

--- arna/__init__.py
+++ arna/__init__.py
@@ -1,9 +1,8 @@
-from core import *
-from campaign_operational_driver import *
-from GEOS import * 
-from campaign_misc import *
-from plotting_fcast import *
-from observations import * .py           
-from plotting import * .py        
-from utils import *
+from . core import *
+from . campaign_operational_driver import *
+from . GEOS import * 
+from . campaign_misc import *
+from . observations import *
+from . plotting import *        
+from . utils import *
 
